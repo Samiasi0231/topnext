6b10dd1a31a7144c320300b2accdd29e86b30a72
unknown <samiasi0231>
2024-08-10 07:02:48 +0100

    my text

--- src/app/page.js
+++ src/app/page.js
@@ -6,20 +6,20 @@ import Link from "next/link";
 const HomePage = () => {
   return (
     <motion.div
-      className="h-full"
+      className="h-fit"
       initial={{ y: "-200vh" }}
       animate={{ y: "0%" }}
       transition={{ duration: "1" }}
     >
-      <section className="flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-lg lg:text-xl">
+            <section className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
         {/* Image container */}
-        <div className="h-64 sm:h-72 lg:h-full lg:w-1/2 relative">
-          <Image
-            src="/asi.png"
-            alt="Description of the image"
-            fill
-            className="object-contain"
-          />
+        <div className="h-1/3 lg:h-full sm:h-fit lg:w-1/2 relative">
+        <Image
+  src="/asi.png"
+  alt="Description of the image"
+  fill
+  className="object-contain"
+/>
         </div>
         {/* Text container */}
         <div className="h-full lg:w-1/2 flex flex-col gap-4 items-center justify-center mt-2 lg:mt-0 lg:ml-8">
